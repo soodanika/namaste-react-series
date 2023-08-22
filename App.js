b5672c76dda6545840d2944e53b073a303459e12
Anika <soodanika2000@gmail.com>
2023-08-22 16:03:30 +0530

    Files for Ep-03

--- App.js
+++ App.js
@@ -1,6 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1",{id:"heading"},"Hello World!");
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
+// const header = React.createElement("div",{className:"title"},[React.createElement("h1",{},"This is h1."),React.createElement("h2",{},"This is h2."),React.createElement("h3",{},"This is h3.")])
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(header);
+
+// const Header = () => (<div className="title">
+//     <h1>This is h1 tag.</h1>
+//     <h2>This is h2 tag.</h2>
+//     <h3>This is h3 tag.</h3>
+// </div>)
+
+// const Otherheader = () =>(
+//     <div>
+//         <h1>Hello from other copmponent.</h1>
+//         <Header/>
+//     </div>
+// )
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Otherheader/>);
+
+
+const Header = () => (
+                        <div className="header-container">
+                            <div className="logo-conatainer">
+                                <img className="logo" alt="logo" src="https://png.pngtree.com/png-vector/20190225/ourmid/pngtree-circuit-logo-template-vector-png-image_704226.jpg"></img>
+                            </div>
+                            <div className="search">
+                                Search: <input type="text" placeholder="Search"/>
+                            </div>
+                            <div className="icons-container">
+                                <img className="icons" alt="user-icon" src="https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small/user-profile-icon-free-vector.jpg"></img>
+                            </div>
+                        </div>
+                    );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Header/>);
---
+++ Assignment-03.md
@@ -0,0 +1,18 @@
+# JSX
+    JSX is a javascript syntax that helps us to write html like code in react.
+    JSX != html
+
+# Superpower of JSX 
+    - allows us to write html like code in javascript.
+    - It is easy to maintain.
+
+# type in script tag 
+    This is used to specify the type of script.
+    text/javascript - specifies that its javascript code.
+    text/ecmascript - specifies that the code follows ecmascript standards
+    module - specifies that its a module that will import or export other files.
+
+# {TitleComponent} vs <TitleComponent/> vs <TitleComponent></Titlecomponent>
+    {TitleComponent} -> this will evaluate the javascript expression or variable.
+   {<TitleComponent/>} vs {<TitleComponent></Titlecomponent>} -> These are different ways of rendering a component.
+
